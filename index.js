25292ea894f9149b46b87dd971b849f43bad4e5f
Maksym7415 <popovmaksim7415@gmail.com>
2021-01-12 22:06:01 +0200

    fixed paymentintent api

--- index.js
+++ index.js
@@ -16,29 +16,14 @@ app.use(bodyParser.urlencoded({
 
 app.post('/sandbox/create-session', async (req, res) => {
   const stripe = Stripe('sk_test_51HGjVfCcIG0MPJSGqWXIyvid7alHehKxkha54GxTEvxdR2U5HDVSNAjLDk5r8FS0SBVRCCxiRUAEYJM3cQ3j1euU00YxPPx5rh');
-  const { name, images, currency, amount, quantity } = req.body;
-  console.log(name, images, currency, amount, quantity)
-  const session = await stripe.checkout.sessions.create({
+  const { currency, amount } = req.body;
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: 1099,
+    currency: 'usd',
     payment_method_types: ['card'],
-    line_items: [
-      {
-        price_data: {
-          currency,
-          product_data: {
-            name,
-            images: [images]
-          },
-          unit_amount: amount,
-        },
-        quantity,
-      },
-    ],
-    mode: 'payment',
-    success_url: 'http://localhost:8000/success', // here should success page
-    cancel_url: 'http://localhost:8000/cancel', // here should be cancel page
   });
 
-  res.json({ id: session.id });
+  res.json({ clientSecret: paymentIntent.client_secret });
 });
 
 app.use('*', (req, res) => res.send('Not found'))
